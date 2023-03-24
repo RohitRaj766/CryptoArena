de777d7b8ca5664381d64a72cffe2f6c9147a3f6
RohitRaj766 <89683588+RohitRaj766@users.noreply.github.com>
2023-03-24 21:20:47 +0530

    Update cryptoApi.js

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -4,8 +4,8 @@ const cryptoApiHeaders = {
     'X-RapidAPI-Key': '665647b101msha91a2b9c70a1f00p1fe60djsn50ceb15b3a0b',
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
 }
-
-const baseUrl =  'https://coinranking1.p.rapidapi.com/coins';
+//1.p.///////oop//
+const baseUrl =  'https://coinranking1.rapidapi.com/coins';
 
 
 const createRequest = (url) => ({
